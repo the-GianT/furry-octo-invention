befb6f9a87b8c44b586127214ddbd995de763114
Gian Tricarico <gtricarico@stuy.edu>
2017-10-15 20:29:51 -0400

    write print_list

--- LList.c
+++ LList.c
@@ -1,4 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-struct node
+struct node {
+  char c;
+  struct node *next
+};
+
+void print_list(struct node * n)
+{
+  printf("[");
+  while (n->c) {
+    printf("%c ", n->c);
+    n = n->next;
+  }
+  printf("]\n");
+}
