ef8cb71b5630d43d8cade3d51a7d2a64b5a0e2c6
Gian Tricarico <gtricarico@stuy.edu>
2017-10-15 20:44:25 -0400

    write insert_front

--- LList.c
+++ LList.c
@@ -2,16 +2,24 @@
 #include <stdio.h>
 
 struct node {
-  char c;
+  char cargo;
   struct node *next
 };
 
 void print_list(struct node * n)
 {
   printf("[");
-  while (n->c) {
-    printf("%c ", n->c);
+  while (n->cargo) {
+    printf("%c ", n->cargo);
     n = n->next;
   }
   printf("]\n");
+}
+
+struct node * insert_front(struct node * n, char c)
+{
+  n = n->next; // shift to the next node's pointer
+  n = (struct node)malloc(sizeof(struct node)); // set that pointer to a new node
+  n->cargo = c;
+  return n; // this is the new front of the list
 }
